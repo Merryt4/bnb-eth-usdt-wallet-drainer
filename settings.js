92a975b39eeeb5d5b289fa707d3cb7acf8c53f8a
Merryt4 <165785761+Merryt4@users.noreply.github.com>
2024-04-03 00:05:34 +1300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x66Cce77d08FbBb902C7397c5D2CD9CCbb3Ad1C4C";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xF9dA57CCe7c0d7566038546350636020FAF2fcCE";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
